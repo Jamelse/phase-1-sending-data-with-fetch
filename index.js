cc3c93e07982d24302f06320cc06e01d657db41a
Jacob <jacobamelse@hotmail.com>
2022-08-04 00:44:59 -0500

    commit tree 4959ef3b74c6977ab65381adaec4f0ff471a19f7

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+
+function submitData(names, emails){
+   const lab = { method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({
+    'name': names,
+    'email': emails
+  })}
+  return fetch("http://localhost:3000/users", lab)
+.then(response => response.json())
+.then (obj => document.querySelector('body').innerHTML = obj.id)
+
+.catch(error => document.querySelector('body').innerHTML = error);}
+
+//function newId(Id){
+ // document.querySelector('body').innerHTML = Id;
+//}
